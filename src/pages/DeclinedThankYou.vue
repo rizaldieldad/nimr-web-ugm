b46ee0ac886da6f59418a38b522ee58df126ee58
rizaldieldad <eldad.rizaldi@gmail.com>
2025-11-06 20:17:04 +0700

    style(DeclinedThankYou): update text styling and layout

--- src/pages/DeclinedThankYou.vue
+++ src/pages/DeclinedThankYou.vue
@@ -6,32 +6,17 @@
     >
         <div class="max-w-6xl mx-auto flex items-center min-h-screen p-10">
             <div class="flex flex-col lg:flex-row items-center justify-center lg:gap-x-5">
-                <p class="text-lg lg:text-4xl text-justify">
-                    {{ $t("thank_you.not_participate") }}
-                </p>
                 <img
-                    class="w-60 h-60 lg:w-100 lg:h-100 object-cover"
+                    class="w-60 h-60 lg:w-100 lg:h-100 object-cover mb-6 lg:mb-0"
                     src="../assets/images/thankyou/thankyou1.png"
                     alt=""
                 />
+                <p class="text-2xl md:text-3xl lg:text-5xl text-[#8ca6e3] font-extrabold">
+                    {{ $t("thank_you.not_participate") }}
+                </p>
             </div>
         </div>   
     </section>
 </template>
 
 <style scoped></style>
-
-<!-- <section
-    class="mx-auto max-w-5xl flex items-center min-h-screen p-10 lg:p-20"
-  >
-    <div class="w-full flex flex-col items-center space-y-20">
-      <img
-        class="w-60 h-60 lg:w-80 lg:h-80 object-cover"
-        src="../assets/images/thankyou1.png"
-        alt=""
-      />
-      <p class="text-lg lg:text-3xl text-justify">
-        {{ $t("thank_you.not_participate") }}
-      </p>
-    </div>
-  </section> -->
