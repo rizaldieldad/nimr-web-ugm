e8eaab23974312c706194e1c6a8e6bff44db8372
rizaldieldad <eldad.rizaldi@gmail.com>
2025-11-07 14:42:48 +0700

    feat(BackButton): create BackButton component

---
+++ src/components/buttons/BackButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useRouter } from "vue-router"
+
+const router = useRouter();
+
+const props = defineProps({
+    backRoute: {
+        type: String,
+        required: true
+    }
+})
+
+const handleBack = () => {
+    router.push(props.backRoute);
+}
+</script>
+
+<template>
+    <div class="flex">
+        <button
+          @click="() => handleBack()"
+          class="w-16 h-16 bg-rose-400 text-white font-semibold rounded-full transition-colors cursor-pointer"
+        >
+          {{ $t("buttons.back") }}
+        </button>
+      </div>
+</template>
+
+<style scoped></style>
