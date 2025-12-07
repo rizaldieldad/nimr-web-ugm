a0654c94e2834bae8deb89866ccb199c20234f17
yogis <yogisyo16@gmail.com>
2025-12-07 07:45:40 +0700

    Update: Button animation on mobile

--- src/components/buttons/NextButton.vue
+++ src/components/buttons/NextButton.vue
@@ -30,7 +30,7 @@ const handleNext = () => {
             'w-16 h-16 border-2 font-semibold rounded-full transition-all duration-200',
             disabled 
               ? 'border-gray-300 bg-gray-100 text-gray-400 cursor-not-allowed opacity-60' 
-              : 'border-sky-500 hover:bg-sky-500 hover:text-white text-pink-400 cursor-pointer hover:scale-105'
+              : 'border-sky-500 hover:bg-sky-500 hover:text-white text-pink-400 cursor-pointer hover:scale-105 active:scale-90 active:bg-sky-600 active:text-white'
           ]"
         >
           {{ $t("buttons.next") }}
